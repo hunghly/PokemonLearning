9f10d8c44401f8128319d6fc2ac59e499daf3ce6
hunghly <hunghly@gmail.com>
2021-02-10 20:11:40 -0600

    added classifier

--- classify.py
+++ classify.py
@@ -18,3 +18,45 @@ ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
 args = vars(ap.parse_args())
 
+# load the image
+image = cv2.imread(args["image"])
+output = image.copy()
+print(image.shape)
+
+# pre-process the image for classification
+image = cv2.resize(image, (96, 96))
+print(image.shape)
+image = image.astype("float") / 255.0
+image = img_to_array(image)
+image = np.expand_dims(image, axis=0)
+# print(image)
+
+# load the trained convolutional neural network and the label
+# binarizer
+print("[INFO] loading network...")
+model = load_model(args["model"])
+print(model.summary())
+lb = pickle.loads(open(args["labelbin"], "rb").read())
+print(lb)
+# # classify the input image
+print("[INFO] classifying image...")
+proba = model.predict(image)[0]
+idx = np.argmax(proba)
+print("probability: ", proba)
+label = lb.classes_[idx]
+print("labels", lb.classes_)
+
+# we'll mark our prediction as "correct" of the input image filename
+# contains the predicted label text (obviously this makes the
+# assumption that you have named your testing image files this way)
+filename = args["image"][args["image"].rfind(os.path.sep) + 1:]
+correct = "correct" if filename.rfind(label) != -1 else "incorrect"
+# build the label and draw the label on the image
+label = "{}: {:.2f}% ({})".format(label, proba[idx] * 100, correct)
+output = imutils.resize(output, width=400)
+cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,
+	0.7, (0, 255, 0), 2)
+# show the output image
+print("[INFO] {}".format(label))
+cv2.imshow("Output", output)
+cv2.waitKey(0)
